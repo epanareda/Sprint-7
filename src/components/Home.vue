c06f36972b31c5e7cea8dca203a20fe6cb56e305
epanareda <epanareda@gmail.com>
2023-02-14 22:49:59 +0100

    exercici 2

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,6 +8,7 @@
                     Una pàgina web (500 €)
                 </label>
             </div>
+            <Panell class="mb-3" v-if="check.includes('web-page')" @pages="setPages" @langs="setLangs"/>
             <div class="form-check mb-3">
                 <input class="form-check-input" type="checkbox" value="seo" id="seo" v-model="check">
                 <label class="form-check-label" for="seo">
@@ -26,8 +27,13 @@
 </template>
 
 <script>
+import Panell from '../components/Panell.vue'
+
 export default {
     name: 'Home',
+    components: {
+        Panell
+    },
     data() {
         return {
             totalPrice: 0,
@@ -36,19 +42,38 @@ export default {
                 "seo": 300,
                 "google-ads": 200,
             },
-            check: []
+            check: [],
+            pages: 0,
+            langs: 0
         }
     },
     computed: {
         price() {
             // As it's asked in the exercice, the total price is saved in a variable inside data, even though for this case it's not necessary.
             this.totalPrice = this.check.reduce((total, name) => total + this.items[name], 0);
+            this.totalPrice += this.pages * this.langs * 30;
             return this.totalPrice;
+        }        
+    },
+    methods: {
+        setPages(n) {
+            this.pages = Number(n);
+        },
+        setLangs(n) {
+            this.langs = Number(n);
+        }
+    },
+    watch: {
+        // Makes sure that the pages and languages are set to 0 once the option of a webpage is removed.
+        check: function(val) {
+            if(!val.includes("web-page")) {
+                this.pages = 0;
+                this.langs = 0;
+            }
         }
     }
 };
 </script>
 
 <style scoped>
-
 </style>
---
+++ src/components/Panell.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <div class="border border-4 border-dark rounded-4 p-4 web-attr-container">
+            <div class="d-flex align-items-center mb-3">
+                <label class="form-label">Nombre de pàgines</label>
+                <input class="form-control" v-model="pagesValue"> <!-- @change="pagesChange($event)"> -->
+            </div>
+            <div class="d-flex align-items-center">
+                <label class="form-label">Nombre d'idiomes</label>
+                <input class="form-control" v-model="languagesValue">
+            </div>
+        </div>
+    </div>
+
+
+
+</template>
+
+<script>
+export default {
+    name: 'Panell',
+    data() {
+        return {
+            pagesValue: "",
+            languagesValue: ""
+        }
+    },
+    // methods: {
+    //     pagesChange(e) {
+    //         console.log(e.target.value);
+    //         this.$emit("page", e.target.value);
+    //     }
+    // },
+    watch: {
+        pagesValue: function(val) {
+            this.$emit("pages", val);
+        },
+        languagesValue: function(val) {
+            this.$emit("langs", val);
+        },
+    }
+};
+</script>
+
+<style scoped>
+    .web-attr-container {
+        width: fit-content;
+    }
+
+    label {
+        width: 10rem;
+    }
+
+    input {
+        width: 10rem;
+    }
+</style>
